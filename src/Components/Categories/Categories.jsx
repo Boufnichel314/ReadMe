2bef7cc9441e8bbc275064b4091b4c58e7e3b046
Boufnichel314 <boufnichel314@gmail.com>
2022-10-01 18:06:14 +0100

    prepare data to map

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import './categories.css'
 import Dostoevsky from './Dostoevsky/Dostoevsky'
+import dost from '../../images/dostoevsky_photo.jpg'
+import Agatha from '../../Components/Categories/Agatha/images/Agatha_Christie.png'
 export default function Categories() {
+  const data = [
+    {
+      name : 'Dostoevsky',
+      profile : dost,
+    },
+    {
+      name : 'Agatha',
+      profile : Agatha,
+    },
+  ]
   return (
     <div className='categories'>
+        <Dostoevsky profile = {dost} Author = 'Fyodor Dostoevsky' data = {data}/>
+        <Dostoevsky profile = {Agatha} Author = 'Agatha Christie' data= {data} />
+        {/* <Dostoevsky/>
         <Dostoevsky/>
         <Dostoevsky/>
-        <Dostoevsky/>
-        <Dostoevsky/>
-        <Dostoevsky/>
-        <Dostoevsky/>
+        <Dostoevsky/> */}
     </div>
   )
 }
--- src/Components/Categories/Dostoevsky/Dostoevsky.jsx
+++ src/Components/Categories/Dostoevsky/Dostoevsky.jsx
@@ -6,7 +6,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import useMediaQuery from '../../useMediaQuery'
-export default function Dostoevsky() {
+export default function Dostoevsky(props) {
     const isMobile = useMediaQuery('(max-width: 768px)')
     const isDesktop = useMediaQuery('(min-width: 960px)');
     const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 960px)');
@@ -28,11 +28,12 @@ export default function Dostoevsky() {
             }
         }, [isMobile, isTablet, isDesktop]
     )
+    console.log(props.data[0].name)
   return (
     <div className='dostoevsky'>
         <div className="type">
-        <h1>Fyodor Dostoevsky</h1>
-        <img className='img' src={require('../../../images/dostoevsky_photo.jpg')} alt="Dostoevsky"/>
+        <h1>{props.Author}</h1>
+        <img className='img' src={props.profile} alt="Dostoevsky"/>
         </div>
         <Swiper
         slidesPerView={nmbr_slides}
@@ -44,75 +45,73 @@ export default function Dostoevsky() {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>
-        <div className="book">            
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/down.jpeg')} alt="Dostoevsky"/>
-            <div className="book-info">
-                <h1>Down and out in paris and london</h1>   
-            </div>
-        </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/homage.jpeg')} alt="Dostoevsky"/>
-            <div className="book-info">
-                <h1>Crime and Punishment</h1>   
-            </div>
-            </div>
-        </SwiperSlide>
+        {
+            props.data.map((item, index) => {
+                return (
+                    <SwiperSlide key={index}>
+                        <div className="book">
+                            <img className='img' src={item.profile} alt="Dostoevsky"/>
+                            <div className="book-info">
+                            <h1>{item.name}</h1>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                )
+            }
+            )}
         <SwiperSlide>
             <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/1984.jpeg')} alt="Dostoevsky"/>
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/الابله.jfif')} alt="Dostoevsky"/>
             <div className="book-info">
-                <h1>Crime and Punishment</h1>   
-            </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/gambler.jpg')} alt="Dostoevsky"/>
-            <div className="book-info">
-                <h1>Crime and Punishment</h1>   
+                <h1>الابله</h1>   
             </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/gambler.jpg')} alt="Dostoevsky"/>
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/the brothers karamazov.jpg')} alt="Dostoevsky"/>
             <div className="book-info">
-                <h1>Crime and Punishment</h1>   
+                <h1>The Brothers Karamazov</h1>   
             </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/gambler.jpg')} alt="Dostoevsky"/>
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/Idiot.jpg')} alt="Dostoevsky"/>
             <div className="book-info">
-                <h1>Crime and Punishment</h1>   
+                <h1>Idiot</h1>   
             </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/gambler.jpg')} alt="Dostoevsky"/>
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/les frères karamazov.jpeg')} alt="Dostoevsky"/>
             <div className="book-info">
-                <h1>Crime and Punishment</h1>   
+                <h1>Les Frères Karamazov</h1>   
             </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/gambler.jpg')} alt="Dostoevsky"/>
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/the gambler.jpg')} alt="Dostoevsky"/>
             <div className="book-info">
-                <h1>Crime and Punishment</h1>   
+                <h1>The Gambler</h1>   
             </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/gambler.jpg')} alt="Dostoevsky"/>
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/Les Démons.jpg')} alt="Dostoevsky"/>
             <div className="book-info">
-                <h1>Crime and Punishment</h1>   
+                <h1>Les Démons</h1>   
+            </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className="book">
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/الاخوة كرامازوف.jfif')} alt="Dostoevsky"/>
+            <div className="book-info">
+                <h1>الاخوة كرامازوف</h1>   
             </div>
             </div>
         </SwiperSlide>
