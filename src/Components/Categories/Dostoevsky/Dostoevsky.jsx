456de34dc1abb73af9adf1692f0eead56c7a86b4
Boufnichel314 <boufnichel314@gmail.com>
2022-10-01 15:56:44 +0100

    add categories style

--- src/Components/Categories/Dostoevsky/Dostoevsky.jsx
+++ src/Components/Categories/Dostoevsky/Dostoevsky.jsx
@@ -11,30 +11,34 @@ export default function Dostoevsky() {
     const isDesktop = useMediaQuery('(min-width: 960px)');
     const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 960px)');
     const [nmbr, setNmbr] = useState(1);
+    const [nmbr_slides, setNmbr_slides] = useState(1);
     useEffect(
         () => {
             if (isMobile) {
-                setNmbr(1)
+                setNmbr(1);
+                setNmbr_slides(1.5);
             }
             if (isTablet) {
-                setNmbr(2)
+                setNmbr(2);
+                setNmbr_slides(2.5);
             }
             if (isDesktop) {
-                setNmbr(3)
+                setNmbr(3);
+                setNmbr_slides(3.5);
             }
         }, [isMobile, isTablet, isDesktop]
     )
   return (
     <div className='dostoevsky'>
         <div className="type">
-        <h1>Fyodor <br /> Dostoevsky</h1>
+        <h1>Fyodor Dostoevsky</h1>
         <img className='img' src={require('../../../images/dostoevsky_photo.jpg')} alt="Dostoevsky"/>
         </div>
         <Swiper
-        slidesPerView={1.5}
+        slidesPerView={nmbr_slides}
         spaceBetween={30}
         slidesPerGroup={nmbr}
-        loop={true}
+        loop={false}
         loopFillGroupWithBlank={true}
         navigation={true}
         modules={[Pagination, Navigation]}
@@ -44,7 +48,7 @@ export default function Dostoevsky() {
         <div className="book">            
             <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/down.jpeg')} alt="Dostoevsky"/>
             <div className="book-info">
-                <h1>Crime and Punishment</h1>   
+                <h1>Down and out in paris and london</h1>   
             </div>
         </div>
         </SwiperSlide>
@@ -66,7 +70,7 @@ export default function Dostoevsky() {
         </SwiperSlide>
         <SwiperSlide>
             <div className="book">
-            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/down.jpeg')} alt="Dostoevsky"/>
+            <img className='img' src={require('../../../Components/Categories/Dostoevsky/images/gambler.jpg')} alt="Dostoevsky"/>
             <div className="book-info">
                 <h1>Crime and Punishment</h1>   
             </div>
