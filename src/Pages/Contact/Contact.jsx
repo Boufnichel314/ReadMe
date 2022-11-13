c095a4487e87738b9e441b3fa76f4bbefe5b0992
Boufnichel314 <boufnichel314@gmail.com>
2022-11-13 21:43:32 +0100

    contact_icons

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,11 @@
 import React, { useRef} from "react";
 import emailjs from "@emailjs/browser";
 import AnimatedPages from '../../Components/AnimatedPages'
+import Container from '@mui/material/Container';
 import { useAlert } from 'react-alert'
 import './contact.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import { useSelector } from "react-redux";
 const Contact = () => {
   const form = useRef();
@@ -40,6 +43,7 @@ const alert = useAlert()
   return (
     <AnimatedPages>
       <div className="contact-page" style = {style}>
+      <Container fixed>
         <div className="contact-form">
         <form ref={form} onSubmit={sendEmail}>
         <div className="input">
@@ -53,11 +57,21 @@ const alert = useAlert()
         </div>
         <div class="wrapper input">
         <input type="submit" value="ارسال" onClick={() => {
-        alert.show('Your message has been sent')
+        alert.show('تم ارسال الرسالة بنجاح')
       }} />
         </div>  
       </form>
+      <div className="icons">
+      <a href="https://www.facebook.com/readme314" target="_blank" rel="noreferrer">
+      <FacebookIcon className="fb"/>
+      </a>
+      <a href="https://wa.me/+212771487686" target="_blank" rel="noreferrer">
+      <WhatsAppIcon className="whts"/>
+      </a>
+      </div>
         </div>
+        
+        </Container>
       </div>
     </AnimatedPages>
   );
